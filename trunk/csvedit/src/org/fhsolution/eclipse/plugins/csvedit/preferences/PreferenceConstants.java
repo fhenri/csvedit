31ba1a7ae9881af27364f0fe878d3b70c69eddf8
frederic.henri <frederic.henri@3a24d86d-b285-eb83-6e90-cdd8df3ba2b4>
2011-01-27 22:21:04 +0000

    fix constants names

--- trunk/csvedit/src/org/fhsolution/eclipse/plugins/csvedit/preferences/PreferenceConstants.java
+++ trunk/csvedit/src/org/fhsolution/eclipse/plugins/csvedit/preferences/PreferenceConstants.java
@@ -8,8 +8,8 @@ package org.fhsolution.eclipse.plugins.csvedit.preferences;
  */
 public class PreferenceConstants {
 
-    public static final String USE_FIRST_LINE_AS_HEADER = "booleanPreference";
-    public static final String CASE_SENSITIVE_SEARCH = "booleanPreference";
-    public static final String CUSTOM_DELIMITER = "stringPreference";
+    public static final String USE_FIRST_LINE_AS_HEADER = "useFirstLinePreference";
+    public static final String CASE_SENSITIVE_SEARCH = "sensitiveSearchPreference";
+    public static final String CUSTOM_DELIMITER = "delimiterPreference";
 
 }
